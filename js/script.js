7c62c1b848508bd60bca64852c08b644bf9265b5
Henry Herrera <henryjherreram@gmail.com>
2020-09-02 13:42:00 -0700

    added contact form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 let resume = document.getElementById('resume');
 let hideResume = document.getElementById('hideResume');
 let showResume = document.getElementById('showResume');
+let buttonMessage = document.getElementById('buttonMessage');
+let contactForm = document.getElementById('contactForm');
+let linkedInBadge = document.getElementById('linkedInBadge');
 
-showResume.onclick = () => {
-    resume.style.display = 'inline-block';
-    hideResume.style.display = 'inline-block';
-    showResume.style.display = 'none';
+
+if (resume) {
+    showResume.onclick = () => {
+        resume.style.display = 'inline-block';
+        hideResume.style.display = 'inline-block';
+        showResume.style.display = 'none';
+    }
+
+    hideResume.onclick = () => {
+        resume.style.display = 'none';
+        hideResume.style.display = 'none';
+        showResume.style.display = 'inline-block';
+    }
 }
 
-hideResume.onclick = () => {
-    resume.style.display = 'none';
-    hideResume.style.display = 'none';
-    showResume.style.display = 'inline-block';
+if (buttonMessage) {
+    buttonMessage.onclick = () => {
+        contactForm.style.display = 'inline-block';
+        contactForm.style.marginTop = '-50px';
+        buttonMessage.style.display = 'none';
+        linkedInBadge.style.display = 'none';
+    }
 }
